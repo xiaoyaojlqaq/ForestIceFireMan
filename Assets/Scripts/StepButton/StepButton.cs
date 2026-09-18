ad4757b0bbabdbbfe79c1929c8ddd4f1f1a70ed5
LOST-DLL <116800519+LOST-DLL@users.noreply.github.com>
2026-09-18 17:14:52 +0800

    按钮

--- Assets/Scripts/StepButton/StepButton.cs
+++ Assets/Scripts/StepButton/StepButton.cs
@@ -6,6 +6,9 @@ public class Button2D : MonoBehaviour
     [SerializeField] private Collider2D buttonCollider;
     [SerializeField] private float rayLength = 0.15f;
     [SerializeField] private LayerMask playerLayer;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private Sprite unpressedSprite;
+    [SerializeField] private Sprite pressedSprite;
 
     [Header("Events")]
     public UnityEvent onPressed;
@@ -23,24 +26,41 @@ public class Button2D : MonoBehaviour
         isPressed = pressed;
 
         if (pressed)
+        {
+            spriteRenderer.sprite = pressedSprite;
             onPressed?.Invoke();
+        }
         else
+        {
+            spriteRenderer.sprite = unpressedSprite;
             onReleased?.Invoke();
+        }
     }
 
     private bool CheckPlayer()
     {
-        Bounds bounds = buttonCollider.bounds;
+        BoxCollider2D box = buttonCollider as BoxCollider2D;
 
-        Vector2 left = new Vector2(bounds.min.x, bounds.max.y);
-        Vector2 right = new Vector2(bounds.max.x, bounds.max.y);
+        if (box == null)
+            return false;
 
-        RaycastHit2D hit = Physics2D.Linecast(
-            left + Vector2.up * rayLength,
-            right + Vector2.up * rayLength,
+        Vector2 center = box.transform.TransformPoint(box.offset);
+        Vector2 up = box.transform.up;
+
+        float halfHeight = box.size.y * 0.5f * box.transform.lossyScale.y;
+
+        Vector2 checkCenter = center + up * (halfHeight + rayLength);
+
+        Collider2D hit = Physics2D.OverlapBox(
+            checkCenter,
+            new Vector2(
+                box.size.x * box.transform.lossyScale.x,
+                0.1f
+            ),
+            box.transform.eulerAngles.z,
             playerLayer
         );
 
-        return hit.collider != null;
+        return hit != null;
     }
 }
