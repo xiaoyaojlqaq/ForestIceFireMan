ab51b13208128e0601a62fe8d21663e36126ec22
xiaoyaojlqaq <536799807@qq.com>
2026-09-20 10:49:17 +0800

    MainUI动效

--- Assets/Scripts/SceneManager/SceneTrigger.cs
+++ Assets/Scripts/SceneManager/SceneTrigger.cs
@@ -1,9 +1,18 @@
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class SceneTrigger : MonoBehaviour
 {
     [SerializeField] private string targetScene;
     public Vector2 changePos;
+    private PlayableDirector mainUI;
+    private void Start()
+    {
+        if (mainUI == null)
+        {
+            mainUI=GameObject.Find("MainUICanvas")?.GetComponent<PlayableDirector>();
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -12,6 +21,7 @@ public class SceneTrigger : MonoBehaviour
 
         SceneTransitionManager.Instance.LoadScene(targetScene, () =>
         {
+            mainUI.Play();
             other.transform.position = changePos;
         });
     }
